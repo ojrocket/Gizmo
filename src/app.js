be01568cedeb641b5b197c85d4f46287264c4790
ojrocket <nsmixd14jashwanth@gmail.com>
2026-06-12 11:28:15 +0530

    added ignore to this file

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send("Hello World! i'm jashwanth you are in acquisition");
+  res.status(200).send('Hello World! i\'m jashwanth you are in acquisition');
 });
 
 export default app;
